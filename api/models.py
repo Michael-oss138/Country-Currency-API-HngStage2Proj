a21c58ccd1958eea8b921f23c25cb1d1ed51edf5
Michael-oss138 <nwaizumichael0@gmail.com>
2025-10-26 23:59:40 +0100

    started defining the models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_lenght=200, unique=True)
+    capital = models.CharField(maz_lenght=200, null=True, blank=True)
+    region = models.CharField(max_lebght=200, null=True, blank=True)
