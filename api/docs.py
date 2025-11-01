2faa607425b5f0b9ada640880ce873662f751589
Michael-oss138 <nwaizumichael0@gmail.com>
2025-11-01 21:57:19 +0100

    a documentation of my journey

---
+++ api/docs.py
@@ -0,0 +1,25 @@
+####
+ 
+#This is not an actual code. I want to use this as a form of documentation of this project.
+#
+#
+#Actually this proj was given a week aho and i am yet to be done due to some reasons.
+#
+#I use a maco 10.13.6, which is a very old macos version which may cause alot of developer limitations which i have been facing for over 3 yerars now. Yh.
+#
+#Well, how i plan to get through this is by working with a collegue of mine to brainstorm this project so we both get to pass this stage, timing has been difficult for us recently though, but then. It's a process i know i'll get through.
+#
+#
+#Basically, the challenge i am facing now is, i use django 5.2 and also working with mysql as the required database for this project.
+#
+#Installing mysql 5.7.23, the best version for my system.
+#
+#Django 5.2 requires a higher veriosn of mysql making me to pause my journey here. 
+#
+#But then i hope to get a better device soon fr so all these would stop, i hope so.
+#
+#for now lets wait for our amazing xit, to be ready so we brainstorm this project together and get pass hngprojsatge2 and move up to the next. i am really confident about moving higher.
+#
+#Backend wizards Let's gooo.
+
+###
